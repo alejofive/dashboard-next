0987c450b8095d4b68b1454bf54d4ff7be76a238
Alejandro Garcia <alejofive555@gmail.com>
2024-04-02 10:14:42 -0500

    feed: arreglo del search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -28,11 +28,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         onChange={(event) => handleSearch(event.target.value)}
-        className="peer block w-full rounded-md border border-gray-400 bg-black-650 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        className="peer block w-full rounded-md border border-gray-400 bg-black-650 py-[9px] pl-10 text-sm text-white  outline-2 placeholder:text-white"
         placeholder={placeholder}
         defaultValue={searchParams.get('query')?.toString()}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-500 " />
     </div>
   );
 }
